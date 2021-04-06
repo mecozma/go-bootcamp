2cd60b0abbda1265db5665f8baf20ac4c2ba4116
eduard <mecozma@gmail.com>
2021-04-06 21:34:38 +0100

    Word finder using Go.

---
+++ 05-section/word-finder/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const corpus = "" +
+	"lazy cat jumps again and again and again "
+
+func main() {
+	words := strings.Fields(corpus)
+	query := os.Args[1:]
+	for _, q := range query {
+		for i, w := range words {
+			if strings.ToLower(q) == w {
+				fmt.Printf("# %-2d: %q\n", i+1, w)
+				break
+			}
+		}
+	}
+}
